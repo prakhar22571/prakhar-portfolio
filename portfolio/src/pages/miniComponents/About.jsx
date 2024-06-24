d43ac95faba1e53f59c522d8e88f86609b3cdedc
prakhar22571 <prakhar.raicic@gmail.com>
2024-06-24 14:20:49 +0530

    Completed

---
+++ portfolio/src/pages/miniComponents/About.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+
+const About = () => {
+  return (
+    <div className="w-full flex flex-col overflow-x-hidden">
+      <div className="relative">
+        <h1
+          className="flex gap-4 items-center text-[2rem] sm:text-[2.75rem] 
+          md:text-[3rem] lg:text-[3.8rem] leading-[56px] md:leading-[67px] 
+          lg:leading-[90px] tracking-[15px] mx-auto w-fit font-extrabold about-h1"
+          style={{
+            background: "hsl(222.2 84% 4.9%)",
+          }}
+        >
+          ABOUT <span className="text-tubeLight-effect font-extrabold">ME</span>
+        </h1>
+        <span className="absolute w-full h-1 top-7 sm:top-7 md:top-8 lg:top-11 z-[-1] bg-slate-200"></span>
+      </div>
+      <div className="text-center">
+        <p className="uppercase text-xl text-slate-400">
+          My Introduction
+        </p>
+      </div>
+      <div>
+        <div className="grid md:grid-cols-2 my-8 sm:my-20 gap-14">
+          <div className="flex justify-center items-center">
+            <img
+              src="/me.jpg"
+              alt="avatar"
+              className="bg-white p-2 sm:p-4 rotate-[15deg] h-[240px] sm:h-[340px] md:h-[350px] lg:h-[450px]"
+            />
+          </div>
+          <div className="flex justify-center flex-col tracking-[1px] text-xl gap-5">
+            <p>
+              My name is Prakhar Rai. I am a student of Indian Institute of Information Technology, Bhagalpur
+              pursuing B.Tech in Computer Science & Engineering [2021 - 2025]. I work as
+              a web developer. My hobbies include watching
+              movies,playing esports, and occasionally cooking.
+            </p>
+            <p className="tracking-[1px] text-xl">
+               My commitment and persistence are essential to me. 
+               I have the courage to tackle any challenges for extended durations.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
